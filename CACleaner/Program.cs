dd53f52c42fe076a79fa69be5d428854373b0b7c
SomeBoringNerd <dev.someboringnerd@protonmail.com>
2022-06-14 13:56:11 +0200

    Initial Commit

---
+++ CACleaner/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+namespace CACleaner
+{
+    class MainClass
+    {
+
+        public static string[] paths = new string[]
+        {
+            @"C:\Windows\Temp",
+            @"C:\Users\" + Environment.UserName + @"\AppData\Local\Temp"
+        };
+
+        public static void Main(string[] args)
+        {
+            resetAsync();
+        }
+
+        private static void resetAsync()
+        {
+            Console.Title = "CACleaner : https://github.com/SomeBoringNerd";
+            Console.ForegroundColor = ConsoleColor.Green; 
+            Console.WriteLine("===========================");
+            Console.WriteLine("");
+            Console.WriteLine("        CACleaner");
+            Console.WriteLine("");
+            Console.WriteLine("===========================");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            foreach(string path in paths)
+            {
+                DirectoryInfo di = new DirectoryInfo(path);
+                if (di.Exists) {
+                    Console.WriteLine("Le dossier " + di.FullName + " existe, on supprime son contenu.");
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        Console.WriteLine("Suppression du fichier " + file.FullName);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Le dossier " + di.FullName + " est vide ou n'existe pas, passons.");
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Fini ! on peut maintenant passer a MalwareByte Cleaner");
+            Console.WriteLine("");
+            Console.WriteLine("Appuyez sur n'importe quelle touche pour continuer.");
+            bool keyRead = false;
+            while (!keyRead)
+            {
+                keyRead = Console.KeyAvailable;
+            }
+        }
+    }
+}
---
+++ CACleaner/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("CACleaner")]
+[assembly: AssemblyDescription("Supprimeur de fichiers temporaires automatique")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Clic&Cie")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("SomeBoringNerd")]
+[assembly: AssemblyTrademark("Open source under the IDC License")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
